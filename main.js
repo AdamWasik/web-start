3e80d0bcb8fb647a4848db2cf6b72a0315ed4d9c
Adam Wasik <adam.aw.wasik@gmail.com>
2021-01-05 13:40:48 +0100

    add change to html file and js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const header = document.querySelector("header");
 const h1 = document.querySelector('h1')
+const p = document.querySelector('p')
 let image = true;
 let title = true;
 
@@ -8,6 +9,7 @@ const changeImage = () => {
     title = !title;
     header.style.backgroundImage = image ? "url(img.jpg)" : "url(img2.jpg)";
     h1.textContent = title ? "image" : "image2"
+    p.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo expedita culpa minima voluptate earum voluptatum excepturi facilis quia? Asperiores, vero voluptatem quis exercitationem architecto maxime saepe ipsam iusto ea quae."
 }
 
 header.addEventListener("click", changeImage)
